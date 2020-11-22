f5e9aee01e4f2ddda3012221eb1aeb84432e328c
Nina-gu <lotusgu9@gmail.com>
2020-11-22 16:20:27 -0600

    entity-Location added

---
+++ src/main/java/com/spare4fun/core/entity/Location.java
@@ -0,0 +1,49 @@
+package com.spare4fun.core.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "Location")
+
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
+    private int id;
+
+    @Column(name = "line_one")
+    @Getter
+    @Setter
+    private String line1;
+
+    @Column(name = "line_two")
+    @Getter
+    @Setter
+    private String line2;
+
+    @Getter
+    @Setter
+    private String city;
+
+    @Getter
+    @Setter
+    private String state;
+
+    @Getter
+    @Setter
+    private String zipcode;
+
+    @Getter
+    @Setter
+    private String country;
+
+
+
+
+}
