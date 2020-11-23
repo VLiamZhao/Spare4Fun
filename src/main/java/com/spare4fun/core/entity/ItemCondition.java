a2a28d7f6d6f8b9ff54cdc2c3d09f477cb5c30ea
Alicewillbe <zhaolonglong4008@gmail.com>
2020-11-22 16:12:43 -0800

    Create ItemCondition.java
    avoid use sql keyword "condition"

---
+++ src/main/java/com/spare4fun/core/entity/ItemCondition.java
@@ -0,0 +1,29 @@
+package com.spare4fun.core.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/** Represents table of item conditions.
+ * @author Xinrong Zhao
+ * @author www.spare4fun.com
+ * @version 1.0
+ * @since 1.0
+ */
+@Entity
+@Table(name = "item_Condition")
+public class ItemCondition implements Serializable {
+    private static final long serialVersionUID = 0L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
+    private int id;
+
+    @Getter
+    @Setter
+    private String label;
+}
