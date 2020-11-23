4f3b515b58a4345113fa48bf0085d34a6f9918ad
joannadengdeng <joannadeng17@gmail.com>
2020-11-22 20:04:49 -0800

    Add java doc description

--- src/main/java/com/spare4fun/core/entity/Category.java
+++ src/main/java/com/spare4fun/core/entity/Category.java
@@ -4,10 +4,19 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
+
+/** Represents a category
+ * @author Qiuyu(Joanna) Deng
+ * @author www.spare4fun.com
+ * @version 1.0
+ * @since 1.0
+ */
 
 @Entity
 @Table(name="category")
-public class Category {
+public class Category implements Serializable {
+    private static final long serialVersionUID = 23432L;
 
     @Id
     @Column(name="category_id")
