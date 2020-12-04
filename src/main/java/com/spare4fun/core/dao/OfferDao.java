4d9bc771b3857c533880f7fe4d6a710da4cf9efd
Alicewillbe <zhaolonglong4008@gmail.com>
2020-12-03 23:42:36 -0800

    mared TODO

--- src/main/java/com/spare4fun/core/dao/OfferDao.java
+++ src/main/java/com/spare4fun/core/dao/OfferDao.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface OfferDao {
     /**
      * save a new offer to offer table
+     * TODO check no duplicate (user, item) pair
      * @param Offer
      * @return Offer that is saved to table
      */
