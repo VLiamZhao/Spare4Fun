e918072948566dc55bc2f8b78bd67e54a4f8c318
Alicewillbe <zhaolonglong4008@gmail.com>
2020-12-08 15:59:16 -0800

    Update ApplicationSecurityConfig.java
    loginPage -> loginProcessingUrl
    -> successHandler
    -> failureHandler
    -> exceptionHandling
    -> cors // cross-origin

--- src/main/java/com/spare4fun/core/security/ApplicationSecurityConfig.java
+++ src/main/java/com/spare4fun/core/security/ApplicationSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.spare4fun.core.security;
 
+import com.google.common.collect.ImmutableList;
 import com.spare4fun.core.service.UserAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -13,14 +16,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 import static com.spare4fun.core.entity.Role.ADMIN;
@@ -40,30 +51,30 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                     .csrf().disable()
+                    .cors()
+                .and()
                     // TODO: change <url> patten match
                     .authorizeRequests()
                     .antMatchers("/user/register", "/search**").permitAll()
                     .anyRequest()
                     .authenticated()
                 .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                .and()
                 .formLogin()
-                    // TODO: change <login>
-                    .loginPage("/user/login")
+                    .loginProcessingUrl("/user/login")
                     .permitAll()
                     .usernameParameter("username")
                     .passwordParameter("password")
-                    .defaultSuccessUrl("/success")
-                    .failureUrl("/fail")
-                    // failure handler?
-                    //.failureHandler(authenticationFailureHandler())
+                    .successHandler(authenticationSuccessHandler())
+                    .failureHandler(authenticationFailureHandler())
                 .and()
                 .rememberMe()
                 .and()
                 .logout()
-                    // TODO: change <logout>
-                    .logoutUrl("/user/logout");
-                    // .logoutSuccessUrl("/login")
-                    // .deleteCookies("JSESSIONID");
+                    .logoutUrl("/user/logout")
+                    .deleteCookies("JSESSIONID");
     }
 
     @Override
@@ -82,10 +93,51 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationFailureHandler authenticationFailureHandler() {
         return new AuthenticationFailureHandler() {
+            // TODO 1: requires to change
             @Override
             public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
-                e.printStackTrace();
+                String message = e.getMessage();
+
+                if(e.getClass() == UsernameNotFoundException.class) {
+                    message = "cannot find a user";
+                } else if(e.getClass() == BadCredentialsException.class) {
+                    message = "check your password";
+                }
+
+                httpServletRequest
+                        .getRequestDispatcher(String.format("/error?message=%s", message))
+                        .forward(httpServletRequest, httpServletResponse);
             }
         };
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new AuthenticationSuccessHandler() {
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                // TODO 1: will replace it by forcing idle time in react frontend
+                HttpSession session = httpServletRequest.getSession();
+                session.setMaxInactiveInterval(5); // set session interval at server side, in seconds
+                // TODO 2: will return informative response
+            }
+        };
+    }
+
+    //@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(ImmutableList.of("*"));
+        configuration.setAllowedMethods(ImmutableList.of("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
