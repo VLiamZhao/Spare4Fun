ee1ad066f281803248527e3d0f2bba6d8a281a99
Alicewillbe <zhaolonglong4008@gmail.com>
2020-12-09 15:27:10 -0800

    Set Up Cross-Origin

--- src/main/java/com/spare4fun/core/CoreApplication.java
+++ src/main/java/com/spare4fun/core/CoreApplication.java
@@ -12,19 +12,4 @@ public class CoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:8080", "http://localhost:3000")
-						.allowedMethods("*")
-						.allowedHeaders("*")
-						.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
-						.allowCredentials(true).maxAge(300);
-			}
-		};
-	}
 }
--- src/main/java/com/spare4fun/core/security/ApplicationSecurityConfig.java
+++ src/main/java/com/spare4fun/core/security/ApplicationSecurityConfig.java
@@ -35,6 +35,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Arrays;
 
 import static com.spare4fun.core.entity.Role.ADMIN;
 import static com.spare4fun.core.entity.Role.USER;
@@ -53,6 +54,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                     .csrf().disable()
+                    .cors()
+                .and()
                     // TODO: change <url> patten match
                     .authorizeRequests()
                     .antMatchers("/user/register", "/search**").permitAll()
@@ -122,5 +125,21 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 // TODO 2: will return informative response
             }
         };
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:8080");
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.addExposedHeader("Access-Control-Allow-Origin");
+        configuration.addExposedHeader("Access-Control-Allow-Credentials");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(300L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
