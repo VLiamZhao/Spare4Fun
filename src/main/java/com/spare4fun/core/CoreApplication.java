55d4f701496973a3b8b104cd5f8d56ce1c311d82
Alicewillbe <zhaolonglong4008@gmail.com>
2020-12-09 15:03:40 -0800

    finished cross origin setting

--- src/main/java/com/spare4fun/core/CoreApplication.java
+++ src/main/java/com/spare4fun/core/CoreApplication.java
@@ -2,6 +2,9 @@ package com.spare4fun.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CoreApplication {
@@ -10,4 +13,18 @@ public class CoreApplication {
 		SpringApplication.run(CoreApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:8080", "http://localhost:3000")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+						.allowCredentials(true).maxAge(300);
+			}
+		};
+	}
 }
--- src/main/java/com/spare4fun/core/security/ApplicationSecurityConfig.java
+++ src/main/java/com/spare4fun/core/security/ApplicationSecurityConfig.java
@@ -27,6 +27,8 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -51,8 +53,6 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                     .csrf().disable()
-                    .cors()
-                .and()
                     // TODO: change <url> patten match
                     .authorizeRequests()
                     .antMatchers("/user/register", "/search**").permitAll()
@@ -122,22 +122,5 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 // TODO 2: will return informative response
             }
         };
-    }
-
-    //@Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(ImmutableList.of("*"));
-        configuration.setAllowedMethods(ImmutableList.of("HEAD",
-                "GET", "POST", "PUT", "DELETE", "PATCH"));
-        // setAllowCredentials(true) is important, otherwise:
-        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
-        configuration.setAllowCredentials(true);
-        // setAllowedHeaders is important! Without it, OPTIONS preflight request
-        // will fail with 403 Invalid CORS request
-        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
